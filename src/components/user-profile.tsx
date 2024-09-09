f63626a2506300272cc819802346d7eb5ac9487b
Rodrigo Bussolo <rbussolo91@gmail.com>
2024-09-09 10:10:54 -0400

    Apenas adicionado a ação de selecionar o arquivo referente a imagem do usuário.

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -20,6 +20,8 @@ const userProfileForm = z.object({
 type UserProfileForm = z.infer<typeof userProfileForm>
 
 export function UserProfile() {
+  const inputFileRef = useRef<HTMLInputElement>(null)
+
   const { data: user, isLoading } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -43,6 +45,14 @@ export function UserProfile() {
       userImage: user?.urlImage ?? logo,
     })
   }, [user, reset])
+
+  function onFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.files)
+  }
+
+  function handleChangeFile() {
+    inputFileRef.current?.click()
+  }
 
   return (
     <>
@@ -79,7 +89,15 @@ export function UserProfile() {
               alt="user"
               className="rounded-full w-[148px] h-[148px] border bg-white"
             />
-            <Button type="button" className="w-full">
+
+            <input
+              className="hidden"
+              ref={inputFileRef}
+              type="file"
+              onChangeCapture={onFileChange}
+            />
+
+            <Button onClick={handleChangeFile} type="button" className="w-full">
               Trocar Foto
             </Button>
           </div>
